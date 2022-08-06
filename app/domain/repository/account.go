b81bc3b98c924fcbeda157b2aaf0168b790825dd
kara9renai <80733629+kara9renai@users.noreply.github.com>
2022-08-07 00:32:06 +0900

    repositroy層の実装

---
+++ app/domain/repository/account.go
@@ -0,0 +1,11 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/kara9renai/yokattar-go/app/domain/object"
+)
+
+type Account interface {
+	FindByUsername(ctx context.Context, username string) (*object.Account, error)
+}
