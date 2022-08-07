0cc687c16d54772bdf4c8a6141e6309184076d25
kara9renai <80733629+kara9renai@users.noreply.github.com>
2022-08-07 14:12:45 +0900

    create router.go

---
+++ app/handler/router.go
@@ -0,0 +1,45 @@
+package handler
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
+	"github.com/kara9renai/yokattar-go/app/app"
+	"github.com/kara9renai/yokattar-go/app/handler/health"
+)
+
+func NewRouter(app *app.App) http.Handler {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(newCORS().Handler)
+
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Mount("/v1/accounts", nil)
+	r.Mount("/v1/health", health.NewRouter())
+
+	return r
+}
+
+func newCORS() *cors.Cors {
+	return cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedHeaders: []string{"*"},
+		AllowedMethods: []string{
+			http.MethodGet,
+			http.MethodHead,
+			http.MethodPut,
+			http.MethodPatch,
+			http.MethodPost,
+			http.MethodDelete,
+			http.MethodOptions,
+		},
+	})
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/kara9renai/yokattar-go
 go 1.17
 
 require (
+	github.com/go-chi/chi v1.5.4
+	github.com/go-chi/cors v1.2.1
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/pkg/errors v0.9.1
