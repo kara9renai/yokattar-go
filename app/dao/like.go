b4e72c74b1fc375ae4e8aab05e7531e75f9139d9
kara9renai <r.enumura@icloud.com>
2023-03-05 09:48:54 -0800

    wip: impl business logic

--- app/dao/like.go
+++ app/dao/like.go
@@ -2,6 +2,9 @@ package dao
 
 import (
 	"context"
+	"database/sql"
+	"errors"
+	"fmt"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/kara9renai/yokattar-go/app/domain/object"
@@ -19,6 +22,32 @@ func NewLike(db *sqlx.DB) repository.Like {
 }
 
 func (r *like) LikeByStatusId(ctx context.Context, accountId int64, statusId int64) (*object.Like, error) {
-	// impl business logic
-	return nil, nil
+	const (
+		insert  = `INSERT INTO like(account_id, status_id) VALUES(?, ?)`
+		confirm = `SELECT id, create_at FROM like WHERE id = ?`
+	)
+	entity := new(object.Like)
+
+	stmt, err := r.db.PreparexContext(ctx, insert)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	result, err := stmt.ExecContext(ctx, accountId, statusId)
+	if err != nil {
+		return nil, err
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return nil, err
+	}
+	err = r.db.QueryRowxContext(ctx, confirm, id).StructScan(entity)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, nil
+		}
+		return nil, fmt.Errorf("%w", err)
+	}
+	return entity, nil
 }
