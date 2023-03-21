927cc9c67df4eb4b99a271dfdc6c78135332d4f9
kara9renai <r.enumura@icloud.com>
2023-03-21 07:52:38 -0700

    画像アップロードAPIがエラーを出していたので修正

--- app/dao/attachment.go
+++ app/dao/attachment.go
@@ -20,16 +20,13 @@ func NewAttachment(db *sqlx.DB) repository.Attachment {
 }
 
 func (r *attachment) UploadFile(ctx context.Context, fileName string) (*object.Attachment, error) {
-
 	const (
-		insert = `INSERT INTO attachment (type, url, description) VALUES (?, ?, ?)`
-
-		confirm = `SELECT * FROM attachment WHERE id = ?`
+		insert  = `INSERT INTO attachment (type, url, description) VALUES (?, ?, ?)`
+		confirm = `SELECT id, type, url, description FROM attachment WHERE id = ?`
 	)
 
-	var entity *object.Attachment
-
-	result, err := r.db.ExecContext(ctx, insert, "image", fileName, "テスト")
+	entity := new(object.Attachment)
+	result, err := r.db.ExecContext(ctx, insert, "image", fileName, "string")
 
 	if err != nil {
 		return nil, err
@@ -38,7 +35,6 @@ func (r *attachment) UploadFile(ctx context.Context, fileName string) (*object.A
 	id, _ := result.LastInsertId()
 
 	err = r.db.QueryRowxContext(ctx, confirm, id).StructScan(entity)
-
 	if err != nil {
 		return nil, fmt.Errorf("%w", err)
 	}
--- app/domain/object/attachment.go
+++ app/domain/object/attachment.go
@@ -2,9 +2,9 @@ package object
 
 type (
 	Attachment struct {
-		ID         int64  `json:"id"`
-		Type       string `json:"type"`
-		URL        string `json:"url"`
-		Desription string `json:"description"`
+		ID          int64  `json:"id"`
+		Type        string `json:"type"`
+		URL         string `json:"url"`
+		Description string `json:"description"`
 	}
 )
