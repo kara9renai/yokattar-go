69a26f250f42b93a01d6d5fa01edbff8dcab787d
kara9renai <r.enumura@icloud.com>
2023-04-22 04:36:08 -0700

    cleanup

--- pkg/server/handler/timelines/home.go
+++ pkg/server/handler/timelines/home.go
@@ -15,33 +15,26 @@ func (h *handler) Home(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
 	account := middleware.AccountOf(r)
-
 	limit, err := request.URLParamOf(r, "limit")
-
 	if err != nil {
 		limit = config.DEFAULT_LIMIT
 	}
-
 	if limit > config.MAX_LIMIT {
 		limit = config.MAX_LIMIT
 	}
 
 	maxId, err := request.URLParamOf(r, "max_id")
-
 	if err != nil {
 		maxId = config.DEFAULT_MAX_ID
 	}
 
 	sinceId, err := request.URLParamOf(r, "since_id")
-
 	if err != nil {
 		sinceId = config.DEFAULT_SINCE_ID
 	}
 
 	t := h.app.Dao.Timeline() // domain/repository の取得
-
 	statuses, err := t.GetHome(ctx, account.ID, maxId, sinceId, limit)
-
 	if err != nil {
 		httperror.InternalServerError(w, err)
 		return
--- pkg/server/handler/timelines/public.go
+++ pkg/server/handler/timelines/public.go
@@ -14,31 +14,25 @@ func (h *handler) Public(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
 	limit, err := request.URLParamOf(r, "limit")
-
 	if err != nil {
 		limit = config.DEFAULT_LIMIT
 	}
-
 	if limit > config.MAX_LIMIT {
 		limit = config.MAX_LIMIT
 	}
 
 	maxId, err := request.URLParamOf(r, "max_id")
-
 	if err != nil {
 		maxId = config.DEFAULT_MAX_ID
 	}
 
 	sinceId, err := request.URLParamOf(r, "since_id")
-
 	if err != nil {
 		sinceId = config.DEFAULT_SINCE_ID
 	}
 
 	t := h.app.Dao.Timeline()
-
 	statuses, err := t.GetPublic(ctx, maxId, sinceId, limit)
-
 	if err != nil {
 		httperror.InternalServerError(w, err)
 		return
